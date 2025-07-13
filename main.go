2c46ecc21792d0d0b84a3849bec930a7f3548c33
anvesha <anvesha@binmile.com>
2025-07-13 20:22:45 +0530

    conflicts merged

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hi there!")
+func main() {
+	nums := []int{2, 3, 5, 0, 2, 4}
+
+	for _, num := range nums {
+		if num%2 == 0 {
+			fmt.Println("N is even")
+		} else {
+			fmt.Println("N is odd")
+		}
+	}
+
 }
